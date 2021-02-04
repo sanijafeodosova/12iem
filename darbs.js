f5171d426a901d6b78190e579cf029c94c42cf34
Dtoce <cape@inbox.lv>
2021-02-04 08:33:15 +0200

    pielikta 3.uzdevums

---
+++ darbs.js
@@ -0,0 +1,16 @@
+function uzd1(){
+    window.location.replace("lapa1.html");
+    //window.open("lapa1.html");
+}
+
+function home(){
+    window.location.replace("index.html");
+
+}
+
+
+
+function uzd2(){
+    window.location.replace("lapa2.html");
+    //šeitmds;jfkn
+}
